27b4c678c74bdf9c18e067ea5a1073074e6fcfa0
mpshohan <mdpatwary377@gmail.com>
2023-01-10 17:53:21 -0500

    Books Create/Index/New

--- myserver.js
+++ myserver.js
@@ -11,6 +11,7 @@ const bodyParser = require('body-parser')
 //routers
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname+'/views')
@@ -31,5 +32,6 @@ db.once('open', () => console.log("Connected to Mongoose!"))
 //use of routers
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
 app.listen(process.env.PORT || 3000)
